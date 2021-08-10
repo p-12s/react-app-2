e21a6c6bc73737b04d01e2d4633da568484ff222
p-12s <mental.notes@yandex.ru>
2021-08-10 22:31:57 +0300

    Добавлен поиск по названию постов

--- src/App.js
+++ src/App.js
@@ -1,32 +1,56 @@
-import React, {useState} from 'react';
+import React, {useState, useMemo} from 'react';
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
 import MySelect from './components/UI/select/MySelect';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
   const [posts, setPosts] = useState([
     {id: 1, title: "This is title", body: "This is body body body"},
   ])
 
+  const [selectedSort, setSelectedSort] = useState('')
+  const sortPost = (sort) => {
+    setSelectedSort(sort)
+  }
+
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const sortedPosts = useMemo(() => {
+    console.log('sort function runnnnnnned')
+    if (selectedSort) {
+      return [...posts].sort((a,b) => a[selectedSort].localeCompare(b[selectedSort]))
+    }
+    return posts
+
+  }, [selectedSort, posts])
+
+  const sortedAndSearchedPost = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
+  
+  }, [searchQuery, sortedPosts])
+
   // функции обратного вызова - их будут вызывать компоненты, т.к. напрямую у них доступа к изменению объекта нет
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
   }
-
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-  const [selectedSort, setSelectedSort] = useState('')
-  const sortPost = (sort) => {
-    setSelectedSort(sort)
-    setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
-  }
+  
+
+  
 
   return (
     <div className="App">
       <PostForm create={createPost}/>
       <div>
+        <MyInput 
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+          placeholder="Search"
+        />
         <MySelect 
           defaultValue="Сортировка"
           option={[
@@ -37,8 +61,8 @@ function App() {
           onChange={sortPost}
         />
       </div>
-      {posts.length
-        ? <PostList remove={removePost} posts={posts} title="This is new titlE"/>
+      {sortedAndSearchedPost.length
+        ? <PostList remove={removePost} posts={sortedAndSearchedPost} title="This is new titlE"/>
         : <h1 style={{textAligne: 'center'}}>Постов нет 😶‍🌫️😶‍🌫️😶‍🌫️</h1>
       }
     </div>

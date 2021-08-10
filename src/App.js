cd2a028baeec5acaab15f09f4b6212b5f83ad97c
p-12s <mental.notes@yandex.ru>
2021-08-10 19:45:38 +0300

    Добавлен пример с передачей props-ов в компоненты

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import Counter from './components/Counter';
-import ClassCounter from './components/ClassCounter';
+import React, {useState} from 'react';
+import PostList from './components/PostList';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: "This is title", body: "This is body body body"},
+    {id: 2, title: "This is title 2", body: "This is body body body 2"},
+    {id: 3, title: "This is title 3", body: "This is body body body 3"},
+    {id: 4, title: "This is title 4", body: "This is body body body 4"},
+  ])
+
   return (
     <div className="App">
-      <Counter/>
-      <ClassCounter/>
+      <PostList posts={posts} title="This is new titlE"/>
     </div>
   );
 }

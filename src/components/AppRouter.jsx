cb327ac011447d7cb9baa96896b257b5e2cda948
p-12s <mental.notes@yandex.ru>
2021-08-12 21:08:46 +0300

    Убран редирект на страницу постов при заргузке детальной страницы поста

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -2,10 +2,15 @@ import React, {useContext} from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { AuthContext } from '../context';
 import { publicRoutes, privateRoutes } from '../router';
+import MyLoader from './UI/loader/MyLoader';
 
 const AppRouter = () => {
-  const {isAuth} = useContext(AuthContext);
-   
+  const {isAuth, isLoading} = useContext(AuthContext);
+  
+  if (isLoading) {
+    return <MyLoader />
+  }
+
   return (
     isAuth
     ? <Switch>

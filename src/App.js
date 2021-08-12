d5eccac0e373ffc574985cc14776790388277e49
p-12s <mental.notes@yandex.ru>
2021-08-12 17:22:17 +0300

    Пагинация вынесена в отдельный компонент

--- src/App.js
+++ src/App.js
@@ -6,19 +6,27 @@ import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/modal/MyModal';
 import MyButton from './components/UI/button/MyButton';
 import MyLoader from './components/UI/loader/MyLoader';
+import MyPagination from './components/UI/pagination/MyPagination';
 import './styles/App.css';
 import PostService from './API/PostService';
 import { useFetching } from './hooks/useFetching';
+import { getPageCount } from './utils/pages';
 
 function App() {
   const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false)
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(5);
+  const [page, setPage] = useState(1);
   const sortedAndSearchedPost = usePost(posts, filter.sort, filter.query)
-  
-  const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts)
+
+  const [fetchPosts, isPostLoading, postError] = useFetching(async (limit, page) => {
+    const response = await PostService.getAll(limit, page );
+    setPosts(response.data)
+
+    const totalCount = response.headers['x-total-count']
+    setTotalPages(getPageCount(totalCount, limit) )
   })
 
   // функции обратного вызова - их будут вызывать компоненты, т.к. напрямую у них доступа к изменению объекта нет
@@ -31,10 +39,14 @@ function App() {
   }
 
   useEffect(() => {
-    fetchPosts()
+    fetchPosts(limit, page)
   }, []) // [] массив зависимостей будет пустым, чтобы функция отработала 1 раз
 
-  
+  const changePage = (page) => {
+    setPage(page)
+    fetchPosts(limit, page)
+  }
+
   return (
     <div className="App">
       <MyButton onClick={fetchPosts} style={{margin: '30px 0'}}>Get post</MyButton>
@@ -50,6 +62,7 @@ function App() {
         ? <div style={{display:"flex", justifyContent: "center", marginTop: 50}}><MyLoader/></div>
         : <PostList remove={removePost} posts={sortedAndSearchedPost} title="This is new titlE"/>
       }
+      <MyPagination page={page} changePage={changePage} totalPages={totalPages}/>
     </div>
   );
 }
